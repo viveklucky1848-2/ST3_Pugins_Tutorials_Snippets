21e0a54aedd3cb9db8631064e1c616bea694c4b2
VS <VIVEKLUCKY249@users.noreply.github.com>
2018-04-27 05:50:29 +0530

    Update TestSublimeTextPlugin.py

--- TestSublimeTextPlugin/TestSublimeTextPlugin.py
+++ TestSublimeTextPlugin/TestSublimeTextPlugin.py
@@ -1,6 +1,6 @@
 #!C:\Python27\python.exe
 
-import sublime, sublime_plugin, random, types, re, os, sys, imp
+import sublime, sublime_plugin, random, types, re, os, sys, imp, subprocess
 # Set Base Path to Plugin's directory
 PLUGIN_BASE_PATH = BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 # Get Windows User folder
@@ -8,7 +8,10 @@ WIN_USER_PATH = BASE_PATH.split('\AppData', 1)[0]
 # Get ST3 Packages path
 ST3_PKG_PATH = BASE_PATH.rsplit('\\',1)[0]
 # Expand Windows Environment Variables
-sublime.message_dialog(os.path.expandvars('%Appdata%'))
+# sublime.message_dialog(os.path.expandvars('%Appdata%'))
+TEST_WIN_PATH = os.path.expandvars('%Appdata%')
+ST_PROJ_PATH = os.path.expandvars('%Userprofile%') + '\\Projects\\simple'
+# subprocess.Popen('explorer '+TEST_WIN_PATH)
 
 class TestSublimeTextPluginCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -19,6 +22,7 @@ class TestSublimeTextPluginCommand(sublime_plugin.TextCommand):
         fileNameExt = fileName.rsplit('.')[1]
         sublimeFuncs = dir(sublime)
         output = '\n'.join(sublimeFuncs)
-        sublime.message_dialog("Vivek")
-        sublime.status_message('Shah')
-        sublime.set_clipboard(output)
+        # sublime.message_dialog("Vivek")
+        # sublime.status_message('Shah')
+        # sublime.set_clipboard(output)
+        subprocess.Popen('explorer '+ST_PROJ_PATH)
