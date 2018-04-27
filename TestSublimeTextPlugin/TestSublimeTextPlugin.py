e2314a3e8a044dcfc0a2aa663db102979a8785dc
VS <VIVEKLUCKY249@users.noreply.github.com>
2018-04-27 05:34:31 +0530

    Create TestSublimeTextPlugin.py

---
+++ TestSublimeTextPlugin/TestSublimeTextPlugin.py
@@ -0,0 +1,24 @@
+#!C:\Python27\python.exe
+
+import sublime, sublime_plugin, random, types, re, os, sys, imp
+# Set Base Path to Plugin's directory
+PLUGIN_BASE_PATH = BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+# Get Windows User folder
+WIN_USER_PATH = BASE_PATH.split('\AppData', 1)[0]
+# Get ST3 Packages path
+ST3_PKG_PATH = BASE_PATH.rsplit('\\',1)[0]
+# Expand Windows Environment Variables
+sublime.message_dialog(os.path.expandvars('%Appdata%'))
+
+class TestSublimeTextPluginCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filePath = self.view.file_name()
+        dirPath = os.path.dirname(filePath)
+        fileName = os.path.basename(self.view.file_name())
+        fileNamePrefix = fileName.rsplit('.')[0]
+        fileNameExt = fileName.rsplit('.')[1]
+        sublimeFuncs = dir(sublime)
+        output = '\n'.join(sublimeFuncs)
+        sublime.message_dialog("Vivek")
+        sublime.status_message('Shah')
+        sublime.set_clipboard(output)
